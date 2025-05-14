067ac37afbdcc2b7fc7ff35d495d3cde73f188dc
Jagadish Nath <84424387+JagadishNath@users.noreply.github.com>
2025-05-14 18:56:42 +0600

    Add files via upload

---
+++ turtle_graphics.py
@@ -0,0 +1,16 @@
+"""This code is about creating graphics using turtle module of python.
+I hope you will enjoy  the output.
+Author: Jagadish Nath
+Date: 14-05-25
+"""
+
+import  turtle
+bob=turtle.Turtle()
+for i  in  range(4):
+    bob.fd(100)
+    bob.lt(90)
+    bob.fd(100)
+    bob.lt(90)
+    bob.fd(100)
+    bob.lt(90)
+    bob.fd(100)
